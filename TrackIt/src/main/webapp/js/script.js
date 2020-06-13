e98877ddb8386b1730c1bca0319644b3d900674e
Iryna <kefa_tretinik@icloud.com>
2020-06-13 17:52:15 -0600

    find by ID, plus deBug, errors

--- TrackIt/src/main/webapp/js/script.js
+++ TrackIt/src/main/webapp/js/script.js
@@ -9,10 +9,12 @@ function init() { // initializes lookup and create new cycle functions
   console.log("We are flowing!");
 
   document.cycleSearch.lookup.addEventListener("click", function (event) {
-    event.preventDefault();
+	event.preventDefault();
+	
     var cycleId = document.cycleSearch.cycleId.value;
     if (!isNaN(cycleId) && cycleId > 0) {
-      getCycle(cycleId);
+	  getCycle(cycleId);
+	  
     }
   });
 // getAll();
@@ -30,18 +32,21 @@ function init() { // initializes lookup and create new cycle functions
 
 function getCycle(cycleId) {
 	
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', `api/cycles/${cycleId}`);
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/cycles/' + cycleId);
 
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState === 4){
 			if(xhr.status === 200){
-				var dataJSON = xhr.responseText; 
-				var data = JSON.parse(dataJSON); 
+				let dataJSON = xhr.responseText; 
+				let data = JSON.parse(dataJSON); 
 				console.log(data);
 				displayCycle(data);
+				
 			}else {
-				if (xhr.status === 404) {					
+				if (xhr.status === 404) {		
+					let dataDiv = document.getElementById('cycleData');
+					dataDiv.textContent = '';	
 					displayError('Record not found');
 				}
 				else {
@@ -57,43 +62,46 @@ function getCycle(cycleId) {
 
 
 function displayCycle(cycle){
-	console.log(cycle);
-	let dataDiv = document.getElementById('cycleData');
+	let ErrorDiv = document.getElementById('ErrorData');
+					ErrorDiv.textContent = '';
+	var dataDiv = document.getElementById('cycleData');
 	dataDiv.textContent = '';
 	
+
 	let name = document.createElement("h1");
-    name.textContent = cycle.name;
+    name.textContent = 'Search Result: ' + cycle.name;
 	  dataDiv.appendChild(name);
 	  let notes = document.createElement('blockquote');
   
-  notes.textContent = cycle.notes;
+  notes.textContent = 'Notes: ' + cycle.notes;
   dataDiv.appendChild(notes);
 	  let periodStart = document.createElement('blockquote');
   
-	  periodStart.textContent = cycle.periodStart;
+	  periodStart.textContent = 'First date of Your last period: ' + cycle.periodStart;
   dataDiv.appendChild(periodStart);
 	  let periodDuration = document.createElement('blockquote');
   
-	  periodDuration.textContent = cycle.periodDuration;
+	  periodDuration.textContent = 'Period Duration(days): ' + cycle.periodDuration;
   dataDiv.appendChild(periodDuration);
 	  let cycleLength = document.createElement('blockquote');
   
-	  cycleLength.textContent = cycle.cycleLength;
+	  cycleLength.textContent = 'Cycle Length(days): ' + cycle.cycleLength;
   dataDiv.appendChild(cycleLength);
 	  let volume = document.createElement('blockquote');
   
-	  volume.textContent = cycle.volume;
+	  volume.textContent = 'Flow volume: ' + cycle.volume;
   dataDiv.appendChild(volume);
 
-  console.log(dataDiv);
-
+ 
 
 
 }
 
 
 function displayError(message){
-	var div = document.getElementById('CycleData');
+	let div = document.getElementById('ErrorData');
+	div.style.color = 'red';
+	div.style.fontFamily = 'American Typewriter';
 	div.textContent = message;
 
 }
